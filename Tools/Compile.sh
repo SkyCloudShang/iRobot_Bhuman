d5868b57c95a815e7e53567fe613593b2d8f3a32
SkyCloudShang <814359194@qq.com>
2017-09-27 00:07:12 +0800

    添加了Tools文件夹,用于存放通用的命令代码

---
+++ Tools/Compile.sh
@@ -0,0 +1,8 @@
+#demo in 2.3.4.1,安装必备的软件
+sudo apt-get install qt5-default libqt5svg5-dev libglew-dev libxml2-dev libasound2-dev clang graphviz
+
+#demo in 2.3.4.1,安装alcommon
+#先cd到Install文件夹下,执行下面的命令,其中"/home/syf/Desktop/"是自己电脑上的文件路径,替换即可
+./installAlcommon  /home/syf/Desktop/naoqi-sdk-2.1.4.13-linux32.tar.gz 
+
+
